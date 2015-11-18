f376ffdf1db2710f619b7b9930484068052a68bd
Tristan Sloughter <t@crashfast.com>
2015-11-18 16:16:11 -0600

    automatic hex publishign and use git tag for version

--- scripts/hex.sh
+++ scripts/hex.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-mkdir ~/.hex
-
+## Setup hex user
+mkdir -p ~/.hex
 echo '{username,<<"'${HEX_USERNAME}'">>}.' > ~/.hex/hex.config
 echo '{key,<<"'${HEX_KEY}'">>}.' >> ~/.hex/hex.config
 
+## Add the rebar3 hex plugin to global
 mkdir -p ~/.config/rebar3
-
 echo '{plugins, [rebar3_hex]}.' > ~/.config/rebar3/rebar.config
 
-./rebar3 hex publish
+./rebar3 hex publish <<EOF
+y
+EOF
