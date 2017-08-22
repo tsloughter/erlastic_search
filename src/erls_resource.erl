7a9e4cbf8405379f729d93c92b47890b573e915b
Facundo Olano <facundo.olano@gmail.com>
2017-08-22 17:25:09 -0300

    Add content-type to requests

--- src/erls_resource.erl
+++ src/erls_resource.erl
@@ -48,7 +48,8 @@ request(State, Method, Path, Headers, Params, Body, Options) ->
                   Props -> <<"?", (encode_query(Props))/binary>>
               end)/binary>>,
     {Headers2, Options1, Body} = make_body(Body, Headers, Options),
-    do_request(State, Method, Path1, Headers2, Body, Options1).
+    Headers3 = default_header(<<"Content-Type">>, <<"application/json">>, Headers2),
+    do_request(State, Method, Path1, Headers3, Body, Options1).
 
 do_request(#erls_params{host=Host, port=Port, timeout=Timeout, ctimeout=CTimeout},
            Method, Path, Headers, Body, Options) ->
